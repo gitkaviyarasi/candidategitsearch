573fda43823de85b92c4440e0f7b4d3644c97847
gitkaviyarasi <kaviyarasikrishnannj@gmail.com>
2024-12-09 18:36:00 -0500

    deploying for render

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import  { useEffect, useState, useMemo } from 'react';
 import type Candidate from '../interfaces/Candidate.interface';
 
 const SavedCandidates = () => {
