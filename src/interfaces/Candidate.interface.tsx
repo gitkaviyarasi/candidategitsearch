153232c1599d99f0249a87c8bed08e4aa9f2e019
gitkaviyarasi <kaviyarasikrishnannj@gmail.com>
2024-12-09 17:07:47 -0500

    tested and working code for retching data from git

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,13 +1,6 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 
 export default interface Candidate{
-// readonly Image:string ;
-// readonly avatar_url:string ;
-// readonly Name:string | null;
-// readonly Location:string | null;
-// readonly Email:string | null;
-// readonly Company:string | null;
-// readonly Bio:string | null;
 
 readonly avatar_url: string;
 readonly name: string;
@@ -16,5 +9,5 @@ readonly location: string;
 readonly email: string;
 readonly company: string;
 readonly bio: string;
-readonly url: string;
+
 }
